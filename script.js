6869eaeb9482475a2f29080c0d8534629396e2a7
Amanda Toop <60271855+atoopdev@users.noreply.github.com>
2022-01-11 18:01:54 -0800

    Output summited task to screen as ul/li

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let submitBTN = document.getElementById("submit-btn");
+let tasksList = document.getElementById("my-tasks-ul");
 
 
 // ------------------addTask----------------------------
@@ -7,7 +8,18 @@ submitBTN.addEventListener("click", addTask)
 function addTask() {
     console.log("Submit button clicked.")
     const task = document.getElementById("task").value;
+
+    // might want to data validate
     const due = document.getElementById("due").value;
 
     console.log(task, due);
+    tasksList.innerHTML +=
+        `<li> 
+    <div>
+        <p><strong>Task: ${task}</strong></p>
+        <p><strong>Due date: </strong>${due}</strong></p>
+        <label for="done">Done:</label>
+        <input type="checkbox" name="done">
+    </div>
+    </li>`;
 }
